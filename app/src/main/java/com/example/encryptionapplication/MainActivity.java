418fb31e9ddf0724ac750e7668fdb42f70129f1c
Raymond Wang <rywang@email.wm.edu>
2020-10-03 16:50:45 -0400

    App can now correctly ask for permissions yee haww

--- app/src/main/java/com/example/encryptionapplication/MainActivity.java
+++ app/src/main/java/com/example/encryptionapplication/MainActivity.java
@@ -1,13 +1,34 @@
 package com.example.encryptionapplication;
 
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.activity.result.contract.ActivityResultContract;
+import androidx.activity.result.contract.ActivityResultContracts;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 
+
+import android.Manifest;
+import android.content.Context;
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
+import android.view.View;
+
+import java.io.File;
 
 public class MainActivity extends AppCompatActivity {
 
     private EncryptionApplication app;
+    /*
+    private ActivityResultLauncher<String> permissionRequester = registerForActivityResult(new ActivityResultContracts.RequestPermission(), isGranted -> {
+        if (isGranted) {
+            goToEncrypt();
+        }
+        else {
+            //ask for a redo lol
+        }
+    });*/
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,6 +42,25 @@ public class MainActivity extends AppCompatActivity {
         startActivity(check_bio);
     }
 
+
+
+    //asks a file browser for a file to encrypt
+
+
+    public void encryptFile(View view){
+        //ask for permissions
+        if (permissionGranted(getApplicationContext(), Manifest.permission.WRITE_EXTERNAL_STORAGE)) {
+            System.out.println("working!");
+            goToEncrypt(null);
+        }
+        else {
+            System.out.println("no permission :(");
+            //permissionRequester.launch(Manifest.permission.WRITE_EXTERNAL_STORAGE);
+            String[] temp = {Manifest.permission.WRITE_EXTERNAL_STORAGE};
+            ActivityCompat.requestPermissions(this, temp ,0);
+        }
+    }
+
     @Override
     protected void onResume() {
         super.onResume();
@@ -30,4 +70,33 @@ public class MainActivity extends AppCompatActivity {
             startActivity(locker);
         }
     }
+
+    private void goToEncrypt(File file){
+        System.out.println("going to encrypt now");
+    }
+
+    //asks a camera app for a picture to encrypt
+    public void encryptFromCamera(View view){
+        //ask for permissions
+        if (permissionGranted(getApplicationContext(), Manifest.permission.CAMERA)) {
+            System.out.println("working!");
+            goToEncrypt(null);
+        }
+        else {
+            System.out.println("no permission :(");
+            //permissionRequester.launch(Manifest.permission.WRITE_EXTERNAL_STORAGE);
+            String[] temp = {Manifest.permission.CAMERA};
+            ActivityCompat.requestPermissions(this, temp ,0);
+        }
+    }
+
+    //asks a file browser for a file to decrypt
+    public void decryptFile(View view){
+
+    }
+
+    public boolean permissionGranted(Context context, String permission){
+        return ContextCompat.checkSelfPermission(context, permission) == PackageManager.PERMISSION_GRANTED;
+    }
+
 }
